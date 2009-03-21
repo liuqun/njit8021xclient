19a7f8e5f5b4a1d74daa4a89df485b926e330dbd
liuqun69 <liuqun69@50b18588-2f3e-42bd-858c-726eac6cfd3f>
2009-03-21 11:32:21 +0000

    修改命令行接口，允许从命令行指定网卡设备名。若使用eth0也可不指定。
    git-svn-id: http://gforge.osdn.net.cn/svn/njit8021xclient/trunk@11 50b18588-2f3e-42bd-858c-726eac6cfd3f

--- main.c
+++ main.c
@@ -19,23 +19,33 @@ int ProcessAuthenticaiton_WiredEthernet(const char *UserName, const char *Passwo
  */
 int main(int argc, char *argv[])
 {
+	char *DeviceName;
+
 	/* 检查当前是否具有root权限 */
 	if (getuid() != 0) {
 		fprintf(stderr, "Sorry, currently %s must be executed as root.\n", argv[0]);
+		fprintf(stderr, "(RedHat/Fedora下使用su命令切换为root)\n");
+		fprintf(stderr, "(Ubuntu/Debian下使用sudo)\n");
 		exit(-1);
 	}
 
 	/* 检查命令行参数格式 */
-	if (argc != 3) {
-		fprintf(stderr, "缺少命令行参数！\n");
-		fprintf(stderr,	"Usage:\n");
+	if (argc<3 || argc>4) {
+		fprintf(stderr, "命令行参数错误！\n");
+		fprintf(stderr,	"正确的调用格式为:\n");
 		fprintf(stderr,	"    %s username password\n", argv[0]);
+		fprintf(stderr,	"    %s username password eth0\n", argv[0]);
+		fprintf(stderr,	"    %s username password eth1\n", argv[0]);
+		fprintf(stderr, "(注：若不指明网卡设备，默认情况下将使用eth0)\n");
 		exit(-1);
+	} else if (argc == 4) {
+		DeviceName = argv[3]; // 允许从命令行指定设备名
+	} else {
+		DeviceName = "eth0"; // 缺省情况下使用的设备
 	}
 
 	/* Process 802.1X authentication through wired Ethernet. */
-	ProcessAuthenticaiton_WiredEthernet(argv[1], argv[2], "eth0");
-	/* Note: 一般情况下使用eth0即可 */
+	ProcessAuthenticaiton_WiredEthernet(argv[1], argv[2], DeviceName);
 
 	return (0);
 }
