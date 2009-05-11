ddd9b2ba1f389c61f1f7a5925087160164ecadcd
liuqun69 <liuqun69@50b18588-2f3e-42bd-858c-726eac6cfd3f>
2009-05-11 06:44:06 +0000

    补充注释，编辑调试信息
    git-svn-id: http://gforge.osdn.net.cn/svn/njit8021xclient/trunk@45 50b18588-2f3e-42bd-858c-726eac6cfd3f

--- RefreshIP.py
+++ RefreshIP.py
@@ -4,7 +4,8 @@
 import dbus
 SystemBus = dbus.SystemBus()
 
-# 查找网卡接口
+# 查找网卡设备
+# (查找对应eth0的HAL设备名)
 oNetworkManager = SystemBus.get_object('org.freedesktop.NetworkManager', '/org/freedesktop/NetworkManager')
 szDeviceList = dbus.Interface(oNetworkManager, 'org.freedesktop.NetworkManager').GetDevices()
 for szDevice in szDeviceList:
@@ -14,6 +15,7 @@ for szDevice in szDeviceList:
 		break
 
 # 查找连接
+# (查找ID为"System eth0"或"Auto eth0"的连接)
 oNetworkManagerSystemSettings = SystemBus.get_object('org.freedesktop.NetworkManagerSystemSettings', '/org/freedesktop/NetworkManagerSettings')
 szConnnectionList = dbus.Interface(oNetworkManagerSystemSettings, 'org.freedesktop.NetworkManagerSettings').ListConnections()
 for szConnection in szConnnectionList:
@@ -22,14 +24,20 @@ for szConnection in szConnnectionList:
 	if szConnectionID == 'System eth0' or szConnectionID == 'Auto eth0':
 		break
 
-# 激活网络接口
-dbus.Interface(oNetworkManager, 'org.freedesktop.NetworkManager').ActivateConnection('org.freedesktop.NetworkManagerSystemSettings', szConnection, szDevice, '/')
-
 # 输出调试信息
 PRINT_DEBUG_MESSAGES = 1
 if PRINT_DEBUG_MESSAGES:
-	print 'Activate network connetion "' + szConnectionID + '"'
-	print '\t' + szConnection
-	print 'Using network interface "' + szNetworkInterface + '"'
-	print '\t' + szDevice
+	print '<debug_msg from="' + __file__ + '" func="' + __name__ + '">'
+	print '  Hal Device: ' + szDevice
+	print '    net.interface = "' + szNetworkInterface + '"'
+	print '  NetworkManager Connection: ' + szConnection
+	print '    connection.id = "' + szConnectionID + '"'
+
+# 激活网络连接
+szActivated = dbus.Interface(oNetworkManager, 'org.freedesktop.NetworkManager').ActivateConnection('org.freedesktop.NetworkManagerSystemSettings', szConnection, szDevice, '/')
+
+# 检查ActivateConnection()的返回值
+if PRINT_DEBUG_MESSAGES:
+	print '  Activated Connection: ' + szActivated
+	print '</debug_msg>'
 
