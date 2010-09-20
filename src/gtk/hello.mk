32964addd6609173f3b561656de68520168b0cc0
Liu Qun <liuqun68@gmail.com>
2010-09-21 06:54:46 +0800

    支持自动扫描po目录，根据已有的po文件创建相应的gmo文件

--- src/gtk/hello.mk
+++ src/gtk/hello.mk
@@ -9,15 +9,8 @@ $(addsuffix $(EXEEXT), hello): $(local_object_files)
 hello.o: CFLAGS+=$(GTK_CFLAGS)
 config.o: CPPFLAGS+=-DLOCALEDIR=\"$(localedir)\"
 #-------------------------------------------------
-lang := zh_CN ja ko en fr de it ru pl
-po_files += $(addsuffix .po, $(lang))
-gmo_files += $(addsuffix .gmo, $(lang))
 hello.pot: $(local_source_files)
 $(po_files): hello.pot
-	if test ! -f $(podir)/$@ ; then \
-		cd $(podir) ; \
-	 	$(MSGINIT) --no-translator --locale=$(basename $(notdir $@)) --input=$(notdir $<) --output-file=$(notdir $@) ; \
-	fi
 	$(MSGMERGE) --quiet --directory=$(podir) --update $(notdir $@) $(notdir $<)
 #-------------------------------------------------
 
