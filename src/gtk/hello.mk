577e3d84128edca017b8a6b867d91ca6dc5c40d4
Liu Qun <liuqun68@gmail.com>
2010-09-21 06:19:58 +0800

    增补make功能，包括以下几项：
    make install-exec
    make install-data
    make install-mo
    make update-po

--- src/gtk/hello.mk
+++ src/gtk/hello.mk
@@ -8,15 +8,20 @@ $(addsuffix $(EXEEXT), hello): LDLIBS+=$(GTK_LIBS)
 $(addsuffix $(EXEEXT), hello): $(local_object_files)
 hello.o: CFLAGS+=$(GTK_CFLAGS)
 config.o: CPPFLAGS+=-DLOCALEDIR=\"$(localedir)\"
+#-------------------------------------------------
+lang := zh_CN ja ko en fr de it ru pl
+po_files += $(addsuffix .po, $(lang))
+gmo_files += $(addsuffix .gmo, $(lang))
 hello.pot: $(local_source_files)
-$(addsuffix .po, $(lang)): hello.pot
+$(po_files): hello.pot
 	if test ! -f $(podir)/$@ ; then \
 		cd $(podir) ; \
-	 	msginit --no-translator --locale=$(basename $(notdir $@)) --input=$(notdir $<) --output-file=$(notdir $@) ; \
+	 	$(MSGINIT) --no-translator --locale=$(basename $(notdir $@)) --input=$(notdir $<) --output-file=$(notdir $@) ; \
 	fi
-	msgmerge --quiet --directory=$(podir) --update $(notdir $@) $(notdir $<)
+	$(MSGMERGE) --quiet --directory=$(podir) --update $(notdir $@) $(notdir $<)
 #-------------------------------------------------
 
 executable_files += $(local_executable_files)
 source_files += $(local_source_files)
 object_files += $(local_object_files)
+xml_files += hello.xml
